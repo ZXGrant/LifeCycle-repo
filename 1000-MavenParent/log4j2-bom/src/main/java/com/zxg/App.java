91b15573149b79fda2b2a3c3ead1cb4176e39a61
ZXGrant <zxg056339@126.com>
2019-09-17 00:15:15 +0800

    添加公共模块

---
+++ 1000-MavenParent/log4j2-bom/src/main/java/com/zxg/App.java
@@ -0,0 +1,13 @@
+package com.zxg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
